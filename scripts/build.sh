0cff5d2c6d0eb4503f33d26def8b3227327a6185
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2019-07-27 12:24:35 +0100

    feat: ignore tests during prod build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,9 @@ rm -rf build
 mkdir -p build/artifact
 
 (cd ./app/background && cp -r ./static ../../build/static)
-(cd ./app/background && yarn build && mv ./build ../../build/background)
-(cd ./app/content && yarn build && mv ./build ../../build/content)
-(cd ./app/popup && yarn build && mv ./build ../../build/popup)
+(cd ./app/background && yarn build && rsync -av --exclude=*/*.test.* ./build ../../build/background)
+(cd ./app/content && yarn build && rsync -av --exclude=*/*.test.* ./build ../../build/content)
+# (cd ./app/popup && yarn build && mv ./build ../../build/popup)
 (cp ./app/manifest.json ./build/manifest.json)
 
 version="$(git rev-parse HEAD | head -c 7)"
