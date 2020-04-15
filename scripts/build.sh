640e51cc2fa2b5e62cfb52d7c8cb8ec2ac76a972
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2020-04-15 22:25:11 +0100

    feat: build common package as part of build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@
 rm -rf build
 mkdir -p build/artifact
 
+(cd ./packages/common && yarn build)
 (cd ./packages/background && yarn build && cp -r ./build ../../build/background)
 (cd ./packages/content && yarn build && cp -r ./build ../../build/content)
 (cd ./packages/popup && yarn build && cp -r ./build ../../build/popup)
